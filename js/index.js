0157d42310cb1153a854bf3806ba46eff209fcaa
Arnold <arnoldtee01@gmail.com>
2025-06-08 23:59:30 +0200

    added about me section, changed title, some code refactoring

--- js/index.js
+++ js/index.js
@@ -23,3 +23,19 @@ function toggleColorMode(mode) {
         body.classList.remove('dark-mode');
     }
 }
+
+// collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
